3c7ca7059a7f624a8a3f4af9004a6315c085b3df
Bogerau <baptiste.ogerau@gmail.com>
2023-10-28 11:40:58 +0200

    création de certains components pour no services

---
+++ src/component/Header/index.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import {
+  Image, 
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Text,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react'
+import { HamburgerIcon, CloseIcon, AddIcon } from '@chakra-ui/icons'
+
+interface Props {
+  children: React.ReactNode
+}
+
+const Links = ['Présentation', 'Nos services']
+
+const NavLink = (props: Props) => {
+  const { children } = props
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={'md'}
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('gray.200', 'gray.700'),
+      }}
+      href={'#'}>
+      {children}
+    </Box>
+  )
+}
+
+export default function WithAction() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Ouvrir le menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Box boxSize='150px'>
+                <Image src='https://image.noelshack.com/fichiers/2023/43/5/1698421717-capture-d-ecran-2023-10-27-a-17-27-19.png' alt='Logo' />
+            </Box>
+            <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+
+      <Box p={4}></Box>
+    </>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
+import WithAction from '@/component/Header'
+import SmallCentered from '@/component/footer'
 import GridListWithHeading from '@/component/présentation'
 import { ChakraBaseProvider, ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <ChakraProvider>
-        <GridListWithHeading></GridListWithHeading>
-        <Component {...pageProps} />
+          <WithAction></WithAction>
+          <GridListWithHeading></GridListWithHeading>
+          <SmallCentered></SmallCentered>
+          <Component {...pageProps} />
       </ChakraProvider>
 }
