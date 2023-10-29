511f78863fce4e8e5d7331996d62498be01eab07
bogerau <baptiste.ogerau@gmail.com>
2023-10-29 18:40:10 +0100

    FIX : carrousel is working now

--- src/component/Carousels/index.tsx
+++ src/component/Carousels/index.tsx
@@ -8,15 +8,14 @@ export default () => {
   return (
     <Swiper
       spaceBetween={50}
-      slidesPerView={3}
+      slidesPerView={1}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
       <SwiperSlide><img src="/images/Maison1.png" alt="première maison" /></SwiperSlide>
       <SwiperSlide><img src="/images/Maison2.png" alt="deuxième maison" /></SwiperSlide>
       <SwiperSlide><img src="/images/Maison3.png" alt="troisième maison" /></SwiperSlide>
-      <SwiperSlide><img src="/images/Maison4.png" alt="troisième maison" /></SwiperSlide>
-      ...
+      <SwiperSlide><img src="/images/Maison4v2.png" alt="troisième maison" /></SwiperSlide>
     </Swiper>
   );
 };
