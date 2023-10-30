b476649cb04ae4196c219401b67a64bf5077997f
Bogerau <baptiste.ogerau@gmail.com>
2023-10-30 18:37:10 +0100

    FIX : Taille bouton de mail contact

--- src/component/formulairecontact/index.tsx
+++ src/component/formulairecontact/index.tsx
@@ -51,7 +51,7 @@ export default function Contact() {
                       <Button
                         size="md"
                         height="48px"
-                        width="200px"
+                        width="300px"
                         variant="ghost"
                         color="#DCE2FF"
                         _hover={{ border: '2px solid #1C6FEB' }}
